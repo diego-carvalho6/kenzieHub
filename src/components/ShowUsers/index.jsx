5c3bb1377ac08e6b7f9046248afd117c06d75077
diego <kiritokirigayakazuto1@gmail.com>
2021-03-26 20:35:30 -0300

    added content

--- src/components/ShowUsers/index.jsx
+++ src/components/ShowUsers/index.jsx
@@ -1,14 +1,46 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import Button from "@material-ui/core/Button";
 import style from "./style.css";
-const ShowUsers = () => {
-  const [nextUrl, setNextUrl] = useState(
-    "https://kenziehub.me/users?perPage=15&page=1"
-  );
+
+import { makeStyles } from "@material-ui/core/styles";
+const useStyles = makeStyles((theme) => ({
+  button: {
+    width: "33%%",
+    minWidth: "250px",
+    margin: "3px",
+  },
+}));
+const ShowUsers = ({ setIsAuth }) => {
+  const classes = useStyles();
+  const history = useHistory();
+  const [nextUrl, setNextUrl] = useState(1);
+  const [next, setNext] = useState("");
+  const [previous, setPrevious] = useState("");
   const [users, setUsers] = useState([]);
-  axios.get(`${nextUrl}`).then((response) => {
-    setUsers(response.data);
+
+  useEffect(() => {
+    axios
+      .get(`https://kenziehub.me/users?perPage=15&page=${nextUrl}`)
+      .then((response) => {
+        setUsers(response.data);
+      });
+  }, [nextUrl]);
+
+  const [token, setToken] = useState(() => {
+    const localToken = localStorage.getItem("token") || "";
+
+    if (!localToken) {
+      return "";
+    }
+    setIsAuth(true);
+    return JSON.parse(localToken);
   });
+  if (!token) {
+    history.push("/register");
+  }
+
   return (
     <>
       <div className="box">
@@ -29,6 +61,43 @@ const ShowUsers = () => {
             </div>
           </div>
         ))}
+        <div style={{ textAlign: "center", width: "100%" }}>
+          {nextUrl <= 0 ? (
+            <Button
+              className={classes.button}
+              disabled="true"
+              variant="contained"
+              color="secondary"
+            >
+              previous page
+            </Button>
+          ) : (
+            <Button
+              className={classes.button}
+              onClick={() => setNextUrl(nextUrl - 1)}
+              variant="contained"
+              color="secondary"
+            >
+              previous page
+            </Button>
+          )}
+          <Button
+            className={classes.button}
+            onClick={() => history.push("/home")}
+            variant="contained"
+            color="primary"
+          >
+            voltar para home
+          </Button>
+          <Button
+            className={classes.button}
+            onClick={() => setNextUrl(nextUrl + 1)}
+            variant="contained"
+            color="secondary"
+          >
+            next page
+          </Button>
+        </div>
       </div>
     </>
   );
